146c58a56ec38118cf01a11a5efbc7fb01d3f02e
Leo Gold <113964474+Leo-Gold1@users.noreply.github.com>
2026-03-03 19:35:18 +0000

    1:05

--- server.js
+++ server.js
@@ -152,4 +152,4 @@ app.get("/sales", async (req, res) => {
 });
 
 // START SERVER
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running ${PORT}`));
